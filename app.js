eeaed0f092fc0466530c54bd7f83fcc4d0630c02
savargb <savargb123@gmail.com>
2026-02-07 13:17:47 +0545

    hiii

---
+++ app.js
@@ -0,0 +1,25 @@
+function clearscreen() {
+    document.getElementById("result").value = "";
+}
+
+function setScreenValue(value) {
+    const r = document.getElementById("result");
+    if (r.value==="Enter an expression" || r.value==="Invalid expression" r.value="")
+        r.value += value;
+}
+
+function calculateResult() {
+    const resultElement = document.getElementById("result");
+    const expression=resultElement.value.trim();
+
+    if(expression==='') {
+        resultElement.value="enter an expression";
+        return;
+    }
+    
+    try {
+        resultElement.value = eval(expression);
+     } catch (e) {
+        resultElement.value="invalid";
+     }
+}
